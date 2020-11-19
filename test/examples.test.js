7ce5cbe592f4eaeb7594e2cc1e18f60f9afb7e03
Robert Dougan <robert@dougan.me>
2020-11-19 11:07:39 -0800

    Fix GH ticket #

--- test/examples.test.js
+++ test/examples.test.js
@@ -170,4 +170,29 @@ describe('examples', function() {
         assert.equal(result.childs[0].uppercaseOther, 'VAL');
       });
   });
+
+  it('nested virtuals that are objects return the value (gh-43)', function() {
+    const schema = new mongoose.Schema({
+      nested: {
+        test: {
+          a: String
+        }
+      }
+    }, { id: false });
+
+    schema.virtual('nested.test2').get(function() {
+      return this.nested.test;
+    });
+
+    schema.plugin(mongooseLeanVirtuals);
+
+    const Model = mongoose.model('gh43c', schema);
+
+    return Model.create({ nested: { test: { a: 'Val' } } }).
+      then(() => Model.findOne().lean({ virtuals: ['nested.test2'] })).
+      then(result => {
+        assert.equal(result.nested.test.a, 'Val');
+        assert.equal(result.nested.test2.a, 'Val');
+      });
+  });
 });
