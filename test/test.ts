3275f41aca02947d6ccbb426ef99b8dfc94ce0c7
Valeri Karpov <val@karpov.io>
2025-05-01 15:48:53 -0400

    test: add TypeScript test for manually typed virtuals re: #74

--- test/test.ts
+++ test/test.ts
@@ -28,3 +28,36 @@ TestModel.findOne().lean<ITest & VirtualsForModel<typeof TestModel>>({ virtuals:
   const name: string = doc.name;
   const nameUpper: string = doc.nameUpper;
 });
+
+function gh74() {
+  interface ITest {
+    name: string;
+  }
+
+  interface TestVirtuals {
+    nameUpper: string;
+  }
+  type TestModel = mongoose.Model<ITest, {}, {}, TestVirtuals>;
+
+  const testSchema = new mongoose.Schema <ITest, TestModel, {}, {}, TestVirtuals>(
+    {
+      name: { type: String, required: true },
+    },
+    {
+      virtuals: {
+        nameUpper: {
+          get() {
+            return this.name.toUpperCase();
+          }
+        }
+      }
+    }
+  );
+
+  const TestModel = mongoose.model('Test', testSchema);
+
+  TestModel.findOne().lean<ITest & VirtualsForModel<typeof TestModel>>({ virtuals: true }).orFail().then(doc => {
+    const name: string = doc.name;
+    const nameUpper: string = doc.nameUpper;
+  });
+}
